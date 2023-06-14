d286eba96f91cd592a154ea2ea5edaae985efc18
Diana <dianaaranda14@gmail.com>
2023-06-14 16:04:47 -0700

    Word count for test.txt

--- wordcount.py
+++ wordcount.py
@@ -1,2 +1,14 @@
 """Count words in file."""
 
+with open("test.txt") as file:
+
+    repetitions = {}
+
+    for line in file:
+        line = line.rstrip().split(" ")
+
+        for word in line:
+            repetitions[word] = repetitions.get(word, 0) + 1
+
+    for keys, values in repetitions.items():
+        print(keys, values)
